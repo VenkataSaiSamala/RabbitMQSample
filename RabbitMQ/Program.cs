7cf798017c8fd8ab8a2b9e62d2d6e491d1fbce78
venkata sai kumar samala <venkat7sai@outlook.com>
2023-04-22 00:28:21 -0400

    inital commit

---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace RabbitMQSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            factory.ClientProvidedName = "Rabbit Mq Sender";
+
+            IConnection conn = factory.CreateConnection();
+            IModel channel = conn.CreateModel();
+
+            string exchangeName = "DemoExchange";
+            string routingkey = "demo-routing-key";
+            string queueName = "DemoQueue";
+
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            channel.QueueDeclare(queueName, false, false, false, null);
+            channel.QueueBind(queueName, exchangeName, routingkey, null);
+
+            for (int i = 0; i < 60; i++)
+            {
+                Console.WriteLine($"Sending mesesage: {i}");
+                byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"RabbitMQ Message: {i}");
+                channel.BasicPublish(exchangeName, routingkey, null, messageBodyBytes);
+
+                Thread.Sleep(1000);
+            }
+
+
+            channel.Close();
+            conn.Close();
+        }
+    }
+}
---
+++ RabbitMQReciever/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQReciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            factory.ClientProvidedName = "Rabbit Mq Sender";
+
+            IConnection conn = factory.CreateConnection();
+            IModel channel = conn.CreateModel();
+
+            string exchangeName = "DemoExchange";
+            string routingkey = "demo-routing-key";
+            string queueName = "DemoQueue";
+
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            channel.QueueDeclare(queueName, false, false, false, null);
+            channel.QueueBind(queueName, exchangeName, routingkey, null);
+
+            //basic quality of service
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, args) =>
+            {
+                Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+
+                var body = args.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine("Message Recieved: {0}", message);
+
+                channel.BasicAck(args.DeliveryTag, false);
+            };
+
+            string consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+            Console.ReadLine();
+
+            channel.Close();
+            conn.Close();
+        }
+    }
+}
